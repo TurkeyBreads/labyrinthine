0a02dc6493993ffa1ffbb7eb7e27fd438e9d44c8
Jia Hao <ssrojh8609@gmail.com>
2026-07-25 12:43:10 +0800

    Create retard.py

---
+++ puzzles/retard.py
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+"""using namespace std;"""
+class std:
+    def               __init__(       self,                        pneumonoultramicroscopicsilicovolcanoconiosis: list       |str|                int    |       bool        |      float|         tuple      | dict     |      list[               list]     |   list[str]           |    list[bool]    |   dict[           list[      int|      bool    ] |dict[      tuple     ,    bool    ,str|               bool], int |     bool |      str])                        :
+        self     .self        =     self        ;self        .self         .self         ,               self       .self    .self          .self     ,     self  .self       .self .self         .self          .self                       .self            .self          .self         .self        .self        .self            .self            .self    .self      .self  .self  .self .self.hippopotomonstrosesquippedaliophobia,     self  .self       .self .self         .self          .self                       .self            .self          .self                 .self        .self            .self            .self    .self      .self  .self  .self .self.hippopotomonstrosesquippedaliophobia  =                     self                             .self             ,                 self      .self                                ,                                                pneumonoultramicroscopicsilicovolcanoconiosis, False
+
+ZeroDivisionError = std(                list                                (map                        (   lambda              x:                             x                                                                                              *2,[1           ,2,3                               ,4])    )   )   
+def      using(namespace:std)                                             : # std::cout << toptenworstcodeofalltime
+    """
+   def      using(namespace:std)                                           : # std:  :cout << toptenworstcodeofalltime
+   
+   
+   
+   lambda              x:                             x                                                                                              *2,[1           ,2,3                               ,4])    )   )   
+def      using(namespace:std)     
+   
+   
+    """
+    WHILETRUE = namespace
+    
+    import random as                                                     math;import math                                                    as                                                                                          time;import time                                                                          as               builtins;import builtins                                                                          as                                                                          __future__;import __future__                     as                                                            numpy;import numpy                                                    as                                                           string; import string                                                                                        as random
+    
+    
+            
+    __start__ = __future__                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      .int
+    def def__init__(slef,orgasm                                                    :std|std|std|                                                          std|list[list[list[list                                 [list[list[list[list[                                                                              list[list[std                                                                           |std|std|list[list[list[                                   list[list[std]]]]                                       ]]            ]               ]   ]                              ]                      ]             ]]                        ]]                             )                                                                   :
+        
+        CONTINUE =                                  slef  .self       .self .self         .self          .self                       .self            .self          .self         .self        .self        .self            .self            .self    .self      .self  .self  .self .self.hippopotomonstrosesquippedaliophobia
+        
+        
+        NIGGA = 10
+        
+        from builtins import len as IndexError,  range as TypeError,  ord as PRINT,  bool as KeyError , int as IndentationError
+        if (not                              not               not   not         not           not   not    not       not    not    not            not      not   not   not          not       not           not   not   not       not                 not   not                   not           not            not    not  not          not    not  not   not      not       not  not    bool(not     not   not    not not    not False                         and  not   not      not   not  not not  not    not True )):
+           
+            while                                                                                                                       WHILETRUE:
+                while                                                                                                                                                                             CONTINUE:
+                                                                                                         NIGGA = 50
+                    # ERROR!
+#                               Traceback (most                                             recent             call                last):
+ # File "<main.py>"                         , line 42,              in <module>
+             # File                 "<main.py>", line                        35, in using
+#NameError:                     name '__future__' is not                           defined.                                Did you                 forget to                      import                          '__future__'
+
+                break
+                break
+            
+        
+        return list([                              [                     KeyError                                    (                         SyntaxError                ) for      SyntaxError                  in         TypeError              (                                                                IndentationError            ([                               10,      20,         30,        40,50           ]              [           0               ]      )              )           ]             for                                     FileNotFoundError                 in    
+        TypeError(                        IndexError([              'BOMBOCLATSKIBIDITOILET'        ]*          PRINT           ('A'
+                 )              ))                    ])
+    __repr__ :std = 0
+    
+    for ERROR                   in                          def__init__(                       ZeroDivisionError,                           None                                           ):
+        for                         PROCEDURE                                in                                 ERROR:
+            __repr__                                     +=                      __start__                         (PROCEDURE)
+    return                           __repr__
+    
+                                 
+print(using(ZeroDivisionError)) 
